cc94e4f37d79b854d060f597b382083607c07ff9
DESKTOP-G53DH8U\suns2 <zium349@daum.net>
2017-04-05 17:02:33 +0900

    UnityProjectInput
    Input

---
+++ Assets/Script/GitTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log("HiGitHubTest!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
